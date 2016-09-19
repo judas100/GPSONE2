55a98565b41342c22271d89bb5834ce5e94e97e0
judas100 <nightmingle@yahoo.com>
2016-09-19 12:18:41 -0300

    Add files via upload

---
+++ www/DialogGPS.js
@@ -0,0 +1,22 @@
+
+
+var DialogGPS = function(message,description,callback,title,buttons){
+
+	var _title = (title || "Usar local?");
+	var _message = (message || "Este app deseja alterar as configurações do seu dispotivo:");
+	var _description = (description || "Use Wi-Fi e redes celulares para localização");
+	var _buttons = (buttons || ["Não","Sim"]);
+
+	if(Object.prototype.toString.call(_buttons) === "[object Array]") {
+
+		cordova.exec(callback,null,'DialogGPS','DISPLAY',[_title,_message,_description,_buttons]);
+			
+	}
+};
+		
+DialogGPS.gpsActive = function(successCallback,failCallback){
+}
+		
+
+
+module.exports = DialogGPS;
